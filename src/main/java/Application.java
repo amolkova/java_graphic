1183a17f0d50ec70ff6b7fb671d594d11e0d2441
amolkova <am7171@mail.ru>
2021-01-29 13:09:01 +0300

    init commit

---
+++ src/main/java/Application.java
@@ -0,0 +1,12 @@
+import frame.MyFrame;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Application {
+    public static void main(String[] args) {
+        MyFrame myFrame = new MyFrame();
+    }
+
+    //Запретить изменение размера окна
+}
